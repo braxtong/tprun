b8637d87b1f391cfe64fb38d7da94089eab76058
braxtong <691458069@qq.com>
2016-08-07 03:56:25 +0800

    Initial Commit

---
+++ tprun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tprun
+//
+//  Created by braxton on 16/8/7.
+//  Copyright © 2016年 braxton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
