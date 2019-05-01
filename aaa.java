f67cbb051e7ac8d21708adf83eebe82e80493847
braxtong <691458069@qq.com>
2019-05-01 19:54:46 +0800

    Create aaa.java

---
+++ aaa.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.core.NestedRuntimeException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class test {
+    public static void main( String[] args ) throws Exception, NestedRuntimeException
+    {
+		//headers
+	        HttpHeaders requestHeaders = new HttpHeaders();
+	        //requestHeaders.add("api-version", "1.0");
+	        requestHeaders.setContentType(MediaType.TEXT_XML);
+	        //requestHeaders.set("Content-Type", "text/xml");
+	        String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" + 
+	        		"<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\r\n" + 
+	        		"  <soap:Body>\r\n" + 
+	        		"    <qqCheckOnline xmlns=\"http://WebXml.com.cn/\">\r\n" + 
+	        		"      <qqCode>string</qqCode>\r\n" + 
+	        		"    </qqCheckOnline>\r\n" + 
+	        		"  </soap:Body>\r\n" + 
+	        		"</soap:Envelope>";
+	        HttpEntity<String> requestEntity = new HttpEntity<String>(xml, requestHeaders);
+
+	        RestTemplate restTemplate = new RestTemplate();
+	        ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://www.webxml.com.cn/webservices/qqOnlineWebService.asmx", requestEntity, String.class);
+	        //ResponseEntity<String> responseEntity = restTemplate.exchange("http://www.webxml.com.cn/webservices/qqOnlineWebService.asmx",HttpMethod.POST,requestEntity,String.class,xml);
+	        System.out.println(responseEntity.getBody());
+    }
+}
